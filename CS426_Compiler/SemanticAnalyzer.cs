5b4dcc1bb3a55a5b43e7107f0938dd15ed0a2f09
cornowen <corn.owen@gmail.com>
2014-11-03 08:45:00 -0700

    Added Semantic Analyzer

---
+++ CS426_Compiler/SemanticAnalyzer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace parser
+{
+    class SemanticAnalyzer : comp5210.analysis.DepthFirstAdapter
+    {
+        System.Collections.Generic.Dictionary<string,parser.Definition>
+            stringhash = new Dictionary<string,Definition>();
+        System.Collections.Generic.Dictionary<comp5210.node.Node, parser.Definition>
+            nodehash = new Dictionary<comp5210.node.Node, Definition>();
+        // before theclass starts, create the two hashes and 
+        // add int and float
+
+
+
+    }
+}
